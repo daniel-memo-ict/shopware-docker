63d166dac71fd0aee42f2e9401fae998c450ea08
Daniel Schipper <daniel@memo-ict.nl>
2025-04-14 12:57:19 +0200

    Add permanent temp vite support?

--- modules/platform-local/admin-watch.sh
+++ modules/platform-local/admin-watch.sh
@@ -5,6 +5,7 @@ LOCAL_PROJECT_ROOT="${CODE_DIRECTORY}/${SHOPWARE_PROJECT}"
 cd "$LOCAL_PROJECT_ROOT" || exit
 PLATFORM_PATH=$(platform_component Administration)
 LOCAL_WEBPACK_CONFIG="${LOCAL_PROJECT_ROOT}/${PLATFORM_PATH}Resources/app/administration/webpack.config.js"
+LOCAL_VITE_CONFIG="${LOCAL_PROJECT_ROOT}/${PLATFORM_PATH}Resources/app/administration/vite.config.mts"
 export USE_SSL_DEFAULT=false
 URL=$(get_url "$SHOPWARE_PROJECT")
 WATCHER_URL="admin-${SHOPWARE_PROJECT}.${DEFAULT_DOMAIN}"
@@ -45,6 +46,31 @@ if [[ -e "${LOCAL_WEBPACK_CONFIG}" ]]; then
             npm run --prefix "$PLATFORM_PATH"/Resources/app/administration/ dev
         exit 0
     fi
+fi
+
+if [[ -e "${LOCAL_VITE_CONFIG}" ]]; then
+  "$0" console "${SHOPWARE_PROJECT}" bundle:dump
+  "$0" console "${SHOPWARE_PROJECT}" feature:dump || true
+
+  echo "Starting watcher at host http://${WATCHER_URL}"
+  docker run \
+      --rm \
+      -it \
+      --name "${SHOPWARE_PROJECT}_admin_watch" \
+      --network shopware-docker_default \
+      -v "$LOCAL_PROJECT_ROOT:/var/www/html" \
+      -e ADMIN_PORT=80 \
+      -e HOST="0.0.0.0" \
+      -e ESLINT_DISABLE=true \
+      -e "APP_URL=$URL" \
+      -e PROJECT_ROOT="/var/www/html" \
+      -e ENV_FILE="/var/www/html/.env" \
+      -e "VIRTUAL_HOST=$WATCHER_URL" \
+      -w "/var/www/html" \
+      --expose 80 \
+      node:"$NODE_VERSION" \
+      npm run --prefix "$PLATFORM_PATH"/Resources/app/administration/ dev
+  exit 0
 fi
 
 compose exec cli bash /opt/swdc/swdc-inside admin-watch "${SHOPWARE_PROJECT}"
