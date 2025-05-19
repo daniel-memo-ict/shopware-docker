e50f56975920c120b7d8be87e2272a68de76ca52
Daniel Schipper <daniel@memo-ict.nl>
2025-05-19 13:07:21 +0200

    Admin build now runs Shopware's own script
    It does everything admin-build did, and more.

--- modules/platform/admin-build.sh
+++ modules/platform/admin-build.sh
@@ -6,20 +6,4 @@ cd "${SHOPWARE_FOLDER}" || exit 1
 
 setup_node_version
 
-export PROJECT_ROOT="${SHOPWARE_FOLDER}"
-export ENV_FILE="${PROJECT_ROOT}/.env"
-
-bin/console bundle:dump
-bin/console feature:dump || true
-
-PLATFORM_PATH=$(platform_component Administration)
-
-if [[ ! -d "${PLATFORM_PATH}/Resources/app/administration/" ]]; then
-  echo "Build failed:"
-  echo "=> Run swdc admin-init ${SHOPWARE_PROJECT} first."
-  exit 1
-fi
-
-npm run --prefix "$PLATFORM_PATH"/Resources/app/administration/ build
-
-bin/console assets:install
+bin/build-administration.sh
